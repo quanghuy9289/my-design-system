e03d37d6d9138353841985fcabf66bc490263b7f
HuyNgo <quanghuy9289@gmail.com>
2020-07-31 16:08:29 +0700

    Storybook deployment with GitHub action

--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { withKnobs, select, boolean } from "@storybook/addon-knobs";
-import { Avartar } from "./Avatar";
+// import React from "react";
+// import { withKnobs, select, boolean } from "@storybook/addon-knobs";
+// import { Avartar } from "./Avatar";
 
-// …
-export default {
-  title: "Design System|Avartar",
-  component: Avartar,
-};
+// // …
+// export default {
+//   title: "Design System|Avartar",
+//   component: Avartar,
+// };
 
-export const knobs = () => (
-  <Avatar
-    loading={boolean("Loading")}
-    size={select("Size", ["tiny", "small", "medium", "large"])}
-    username="Dominic Nguyen"
-    src="https://avatars2.githubusercontent.com/u/263385"
-  />
-);
+// export const knobs = () => (
+//   <Avatar
+//     loading={boolean("Loading")}
+//     size={select("Size", ["tiny", "small", "medium", "large"])}
+//     username="Dominic Nguyen"
+//     src="https://avatars2.githubusercontent.com/u/263385"
+//   />
+// );
 
-knobs.story = {
-  decorators: [withKnobs],
-};
+// knobs.story = {
+//   decorators: [withKnobs],
+// };
